cd633983c5b8b0da48fc16d74f2cf0a3c420ddfb
tjaouen <trjaouen@laposte.net>
2016-06-19 15:45:49 +0200

    Fin du modèle simple avec retirement d'aéroport et itérations en paramètres

--- citySIR_traitement_bycity.R
+++ citySIR_traitement_bycity.R
@@ -16,8 +16,8 @@ for (i in 1:length(city)){
 
   x11()
 #  par(mfrow=c(1,2))
-  plot(S~time,type='l',main=c('Profil SIR pour',city[i]))
-  
-  lines(I~time,col='red')
-  lines(R~time,col='blue')
+  plot(S~time,type='l',main=c('Profil SIR pour',city[i]),xlab='Time',ylab='Population',lwd=1,ylim=c(0,max(max(S),max(I),max(R))))
+  lines(I~time,col='red',lwd=1)
+  lines(R~time,col='blue',lwd=1)
+  legend('topright',legend=c('Sain','Infectés','Résistants'),col=c("black", "red","blue"),lwd=2)
 }
